17fb34b040fe05dce1aa49e8967a4135476f077b
dewaleolaoye <adewaleolaoye21@gmail.com>
2021-08-07 23:15:18 +0100

    feat: basic styles

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Search from './Search';
 function App() {
   return (
     <div className='App'>
-      <h1>Hello</h1>
-
       <Search />
     </div>
   );
--- src/Search.jsx
+++ src/Search.jsx
@@ -3,12 +3,14 @@ import debounce from 'lodash.debounce';
 
 const Search = () => {
   const [user, setUser] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleSearch = async (e) => {
     const query = e.target.value;
-    const page = 10;
+    const page = 12;
 
     if (e.target.value.length > 1) {
+      setLoading(true);
       const API = `https://api.github.com/search/users?q=${query}&per_page=${page}`;
       try {
         const response = await fetch(API);
@@ -16,10 +18,13 @@ const Search = () => {
         const data = await response.json();
 
         setUser(data.items);
+        setLoading(false);
       } catch (error) {
+        setLoading(false);
         console.log(error, 'ERROR');
       }
     } else {
+      setLoading(false);
       setUser([]);
     }
   };
@@ -31,22 +36,28 @@ const Search = () => {
   }, []);
 
   return (
-    <div>
-      <input
-        onChange={debouncedChangeHandler}
-        type='text'
-        placeholder='search github user'
-      />
-
-      {user.length > 0 ? (
-        user.map(({ avatar_url, login, id }) => (
-          <div key={id}>
-            <img src={avatar_url} alt={login} />
-          </div>
-        ))
-      ) : (
-        <p>Search for a user</p>
-      )}
+    <div className='search'>
+      <h1>Search for a github user</h1>
+      <div>
+        <input
+          onChange={debouncedChangeHandler}
+          type='text'
+          placeholder='search github user'
+        />
+        {loading && <span>Loading...</span>}
+      </div>
+      <div className='search-result'>
+        {user.length > 0
+          ? user.map(({ avatar_url, login, id, html_url }) => (
+              <div key={id}>
+                <a href={html_url} target='_blank' rel='noopener noreferrer'>
+                  <img src={avatar_url} alt={login} />
+                </a>
+                <p>{login}</p>
+              </div>
+            ))
+          : null}
+      </div>
     </div>
   );
 };
